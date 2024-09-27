d7018ceb619d5867f74d3524a9e3a296a3a5a812
MUSTAFA ÇAY <mcay51@gmail.com>
2024-09-27 20:22:41 +0300

    First commit

---
+++ HibernateLazyAndEager/src/main/java/tr/com/mcay/hibernatelazyandeager/HibernateLazyAndEagerApplication.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.hibernatelazyandeager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateLazyAndEagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateLazyAndEagerApplication.class, args);
+    }
+
+}
---
+++ HibernateOptimization/src/main/java/tr/com/mcay/hibernateoptimization/HibernateOptimizationApplication.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.hibernateoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateOptimizationApplication.class, args);
+    }
+
+}
